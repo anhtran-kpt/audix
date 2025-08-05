1ed706fef96ba4b8cb57405add47b28e1e52d51e
anhtran-kpt <anhtr.work@gmail.com>
2025-08-05 10:39:16 +0700

    Chore: Fix eslint config error and seed data for neon database

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+import { defineConfig } from "eslint/config";
+
 export default defineConfig([
   {
     ignores: ["src/app/generated/prisma/**"],
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,120 +7,127 @@ import { songs as songData } from "./songs.json";
 import { AlbumType, ArtistRole, CreditRole } from "@/app/generated/prisma";
 
 async function main() {
-  await prisma.$transaction(async (tx) => {
-    await tx.genre.createMany({
-      data: genreData,
-      skipDuplicates: true,
-    });
-
-    const allGenres = await tx.genre.findMany({
-      select: { id: true, slug: true },
-    });
-
-    const genreMap = Object.fromEntries(allGenres.map((g) => [g.slug, g.id]));
-
-    const artistsPure = artistData.map(({ genreSlugs, ...rest }) => rest);
-
-    await tx.artist.createMany({
-      data: artistsPure,
-      skipDuplicates: true,
-    });
-
-    const allArtists = await tx.artist.findMany({
-      select: { id: true, slug: true },
-    });
-
-    const artistMap = Object.fromEntries(allArtists.map((a) => [a.slug, a.id]));
-
-    const albumsPure = albumData.map(
-      ({ genreSlugs, artistSlug, albumType, releaseDate, ...rest }) => ({
-        ...rest,
-        artistId: artistMap[artistSlug],
-        albumType: albumType as AlbumType,
-        releaseDate: new Date(releaseDate),
-      })
-    );
-
-    await tx.album.createMany({
-      data: albumsPure,
-      skipDuplicates: true,
-    });
-
-    const allAlbums = await tx.album.findMany({
-      select: { id: true, slug: true },
-    });
-
-    const albumMap = Object.fromEntries(allAlbums.map((a) => [a.slug, a.id]));
-
-    const songsPure = songData.map((s) => ({
-      title: s.title,
-      slug: s.slug,
-      audioId: s.audioId,
-      duration: s.duration,
-      trackNumber: s.trackNumber,
-      isExplicit: s.isExplicit,
-      albumId: albumMap[s.albumSlug],
-    }));
-
-    await tx.song.createMany({
-      data: songsPure,
-      skipDuplicates: true,
-    });
-
-    const allSongs = await tx.song.findMany({
-      select: { id: true, slug: true },
-    });
-    const songMap = Object.fromEntries(allSongs.map((s) => [s.slug, s.id]));
-
-    await tx.songArtist.createMany({
-      data: songData.flatMap((s) =>
-        s.artists.map((ar) => ({
-          songId: songMap[s.slug],
-          artistId: artistMap[ar.slug],
-          role: ar.role as ArtistRole,
-          order: ar.order,
-        }))
-      ),
-      skipDuplicates: true,
-    });
-
-    await tx.songGenre.createMany({
-      data: songData.flatMap((s) =>
-        s.genreSlugs.map((gs) => ({
-          songId: songMap[s.slug],
-          genreId: genreMap[gs],
-        }))
-      ),
-      skipDuplicates: true,
-    });
-
-    await tx.songCredit.createMany({
-      data: songData.flatMap((s) =>
-        s.credits.map((c) => ({
-          songId: songMap[s.slug],
-          artistId: c.slug ? artistMap[c.slug] : null,
-          name: c.name || "",
-          role: c.role as CreditRole,
-          order: c.order,
-        }))
-      ),
-      skipDuplicates: true,
-    });
-
-    for (const alb of albumData) {
-      const relatedSongs = songData.filter((s) => s.albumSlug === alb.slug);
-      const totalTracks = relatedSongs.length;
-      const duration = relatedSongs.reduce((sum, s) => sum + s.duration, 0);
-
-      await tx.album.update({
-        where: { slug: alb.slug },
-        data: {
-          totalTracks,
-          duration,
-        },
+  await prisma.$transaction(
+    async (tx) => {
+      await tx.genre.createMany({
+        data: genreData,
+        skipDuplicates: true,
       });
+
+      const allGenres = await tx.genre.findMany({
+        select: { id: true, slug: true },
+      });
+
+      const genreMap = Object.fromEntries(allGenres.map((g) => [g.slug, g.id]));
+
+      const artistsPure = artistData.map(({ genreSlugs, ...rest }) => rest);
+
+      await tx.artist.createMany({
+        data: artistsPure,
+        skipDuplicates: true,
+      });
+
+      const allArtists = await tx.artist.findMany({
+        select: { id: true, slug: true },
+      });
+
+      const artistMap = Object.fromEntries(
+        allArtists.map((a) => [a.slug, a.id])
+      );
+
+      const albumsPure = albumData.map(
+        ({ genreSlugs, artistSlug, albumType, releaseDate, ...rest }) => ({
+          ...rest,
+          artistId: artistMap[artistSlug],
+          albumType: albumType as AlbumType,
+          releaseDate: new Date(releaseDate),
+        })
+      );
+
+      await tx.album.createMany({
+        data: albumsPure,
+        skipDuplicates: true,
+      });
+
+      const allAlbums = await tx.album.findMany({
+        select: { id: true, slug: true },
+      });
+
+      const albumMap = Object.fromEntries(allAlbums.map((a) => [a.slug, a.id]));
+
+      const songsPure = songData.map((s) => ({
+        title: s.title,
+        slug: s.slug,
+        audioId: s.audioId,
+        duration: s.duration,
+        trackNumber: s.trackNumber,
+        isExplicit: s.isExplicit,
+        albumId: albumMap[s.albumSlug],
+      }));
+
+      await tx.song.createMany({
+        data: songsPure,
+        skipDuplicates: true,
+      });
+
+      const allSongs = await tx.song.findMany({
+        select: { id: true, slug: true },
+      });
+      const songMap = Object.fromEntries(allSongs.map((s) => [s.slug, s.id]));
+
+      await tx.songArtist.createMany({
+        data: songData.flatMap((s) =>
+          s.artists.map((ar) => ({
+            songId: songMap[s.slug],
+            artistId: artistMap[ar.slug],
+            role: ar.role as ArtistRole,
+            order: ar.order,
+          }))
+        ),
+        skipDuplicates: true,
+      });
+
+      await tx.songGenre.createMany({
+        data: songData.flatMap((s) =>
+          s.genreSlugs.map((gs) => ({
+            songId: songMap[s.slug],
+            genreId: genreMap[gs],
+          }))
+        ),
+        skipDuplicates: true,
+      });
+
+      await tx.songCredit.createMany({
+        data: songData.flatMap((s) =>
+          s.credits.map((c) => ({
+            songId: songMap[s.slug],
+            artistId: c.slug ? artistMap[c.slug] : null,
+            name: c.name || "",
+            role: c.role as CreditRole,
+            order: c.order,
+          }))
+        ),
+        skipDuplicates: true,
+      });
+
+      for (const alb of albumData) {
+        const relatedSongs = songData.filter((s) => s.albumSlug === alb.slug);
+        const totalTracks = relatedSongs.length;
+        const duration = relatedSongs.reduce((sum, s) => sum + s.duration, 0);
+
+        await tx.album.update({
+          where: { slug: alb.slug },
+          data: {
+            totalTracks,
+            duration,
+          },
+        });
+      }
+    },
+    {
+      timeout: 30000,
     }
-  });
+  );
 
   console.log("🌱 Seeding completed!");
 }
